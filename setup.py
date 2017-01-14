5a962799f59522f43594ba6545b1e8de7bef178a
shubhajeet2107 <subhajeet.dey@girnarsoft.com>
2017-01-14 12:46:44 +0530

    Start Django SimplePush wth barebones project files

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="django-simplepush",
+    version="0.10",
+    description="A webpush notifications plugable app for Django",
+    long_description=read('README.rst'),
+    url='https://github.com/shubhajeet2107/django-simplepush',
+    license='BSD',
+    author='Subhajeet Dey',
+    author_email='subhajeet2107@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        'Environment :: Console',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Notifications'
+    ]
+)
